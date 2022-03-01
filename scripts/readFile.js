822ad7b671a8132d99a7da8745b1679ba574df72
Bromain <377roro@gmail.com>
2022-03-01 16:58:05 +0100

    amélioration du fichier readFile

---
+++ scripts/readFile.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const promisify = require('util').promisify
+
+
+async function readFile () {
+  
+}
+
+export default readFile;
--- scripts/unzipFile.js
+++ scripts/unzipFile.js
@@ -4,15 +4,24 @@ const fs = require('fs');
 const promisify = require('util').promisify
 const deleteDir = promisify(fs.rmdir)
 
-async function unzipFile (source = '/Users/romainbuisson/Documents/cours/B3/dev_dekstop/example-presentation.codeprez') {
-    try {
-        await deleteDir(path.resolve('./temp'), { recursive: true, force: true })
+async function unzipFile (source) {
+  // source = '/Users/romainbuisson/Documents/cours/B3/dev_dekstop/example-presentation.codeprez'
+  if (!source) return false;
+
+  try {
+    await deleteDir(path.resolve('./temp'), { recursive: true, force: true })
+  } catch (error) {
+    console.error(error)
+  }
+
+  try {
         await extract(path.resolve(source), { dir: path.resolve('./temp') })
         console.log('Extraction complete')
       } catch (err) {
         console.error(err)
-        return ;
+        return false;
       }
+  return true;
 }
 
 export default unzipFile;
